4f85c9ee6eb9f6dfb6c93b851ab60a9e556ccafe
Akshay Pandit <panditakshay402@gmail.com>
2024-11-03 14:52:38 +0530

    free from layout file

--- SMS/Controllers/HomeController.cs
+++ SMS/Controllers/HomeController.cs
@@ -10,21 +10,8 @@ namespace SMS.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View("Index");
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+                
     }
 }
